1b200ab5e1cbedba038b247d1bd3f98f858b767c
MagmMe <marcin@magm.me>
2020-07-14 08:24:58 +0200

    Initial Commit

---
+++ Calculator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Calculator
+//
+//  Created by Marcin Magiera on 14/07/2020.
+//  Copyright © 2020 Magme Agency. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
